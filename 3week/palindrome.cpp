12a0d2ebe6c6d4b9360d861bcef02b974a4d205d
heeseung <tlsgmltmd726529@gmail.com>
2021-10-17 19:32:04 +0900

    3week

--- 3week/palindrome.cpp
+++ 3week/palindrome.cpp
@@ -1,24 +1,26 @@
+/*!
+ *     Copyright:  Copyright (c) 2021, HeeSeung.me
+ */
+
 #include <iostream>
 #include <string>
-#include "palindrome.h"
+#include "/mnt/c/Users/shs72/desktop/Object_Oriented_Design/3week/palindrome.h"
 
-int countPalindrome(unsigned int min, unsigned int max){
-	int count = 0;
-	for(int i=min;i<=max;i++){
-		std::string str = std::to_string(i);
-		int leng = str.length();
-		bool isPalindrome = true;
-		for(int j=0;j<leng/2;j++)
-		{
-			if(str[j] != str[leng -j-1]){
-				isPalindrome = false;
-				break;
-			}			
-		}
-		if(isPalindrome){
-			count = count + 1;
-		}
-	}
-	return count;
-
+int countPalindrome(unsigned int min, unsigned int max) {
+        int count = 0;
+        for (int i = min; i <= max; i++) {
+                std::string str = std::to_string(i);
+                int leng = str.length();
+                bool isPalindrome = true;
+                for (int j = 0; j < leng/2; j++) {
+                        if (str[j] != str[leng -j-1]) {
+                                isPalindrome = false;
+                                break;
+                        }
+                }
+                if (isPalindrome) {
+                        count = count + 1;
+                }
+        }
+        return count;
 }
